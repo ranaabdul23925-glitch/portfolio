66b0033cf68f4d0376e3b2e057e1dc38ac82333f
ranaabdul23925-glitch <ranaabdul23925@gmail.com>
2026-09-25 04:50:51 -0700

    fix pino http

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -7,17 +7,17 @@ import { logger } from "./lib/logger";
 const app: Express = express();
 
 app.use(
-  pinoHttp({
+  pinoHttp.default({
     logger,
     serializers: {
-      req(req) {
+      req(req: any) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+      res(res: any) {
         return {
           statusCode: res.statusCode,
         };
@@ -25,6 +25,7 @@ app.use(
     },
   }),
 );
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
